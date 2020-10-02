ca7c2799a8dde5ebbdde27488090bf298d2b2d0b
slydiesel <59974349+slydiesel@users.noreply.github.com>
2020-10-02 15:53:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
